b4e07b21f785efcda34c3a4f314ab67f9da9f1b9
yuruiqin-gif <110699539+yuruiqin-gif@users.noreply.github.com>
2024-05-30 13:06:33 -0600

    Day 2
    Worked on incorporating the timer function into the program to time the user during the game. Encountered a infinite loop, but should be a simple fix.

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@
 let startButton;
 let easyMode;
 
+var timerValue = 10;
+
 function setup() {
   background(220);
   createCanvas(windowWidth, windowHeight);
@@ -38,8 +40,42 @@ function GameStart() {
   easyMode.position(windowWidth/2, windowHeight/2);
 }
 
-function playEasy() { //Easy Mode
+function timer(){
+  textAlign(CENTER, CENTER);
+  textSize(100);
+  text(timerValue, width/2, height/2);
+  while(timerValue != 0){
+    if (frameCount % 60 == 0 && timerValue > 0){
+      timerValue -= 1;
+    }
+  }
+  if (timerValue == 0){
+    text('FINISHED',50,50);
+  }
+}
 
+function playEasy() { //Easy Mode
+  background(220);
+  easyMode.remove();
+  timer();
   //chemical formulas
   let carbonDioxide = "";
 }
+
+// var timerValue = 100;
+// function setup() {
+//   createCanvas(400, 400);
+// }
+
+// function draw() {
+//   background(255);
+//   textAlign(CENTER, CENTER);
+//   textSize(100);
+//   text(timerValue, width/2, height/2);
+//   if (frameCount % 60 == 0 && timerValue > 0){
+//     timerValue -= 1;
+//   }
+//   if (timerValue == 0){
+//     text('FINISHED',width/2, height/2);
+//   }
+// }
