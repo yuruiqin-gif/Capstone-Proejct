ff28ee24a93ee716cae6aaeba72000916da20e47
Yurui <yuruiqin188@gmail.com>
2024-06-15 23:46:26 -0600

    Day 8
    Instead of making this a program to study lewis dot structures, I made it to study bohr models to aid a larger audience of students. Added function that adds new shell when mouse is clicked.

--- sketch.js
+++ sketch.js
@@ -16,172 +16,218 @@
 // let y = 200;
 
 
+let shells = [];
+
 function setup(){
-  // easyMode.remove();
-
-  //chemical formulas
-  // let carbonDioxide = "";
-
   createCanvas(windowWidth, windowHeight);
-  background(102);
+  stroke(0);
+  noFill();
 
-  startButton = createButton("Submit Atoms");
-  startButton.mousePressed(submitAtoms);
-  startButton.style("font-family", "Optima bold");
+  //Intro Screen Button
+  startButton = createButton("Click To Start");
+  startButton.mousePressed(GameStart);
+
   startButton.style("font-size", "30px");
-  startButton.position(windowWidth/2-75, windowHeight/2);
-  //Hydrogen
-  let hydrogen = createDiv('H');
-  hydrogen.position(100,windowHeight-300);
-  hydrogen.size(50,50);
-  hydrogen.style('font-size', '50px');
-  hydrogen.draggable();
-
-  //Lithium
-  let lithium = createDiv('Li');
-  lithium.position(100,windowHeight-250);
-  lithium.size(50,50);
-  lithium.style('font-size', '50px');
-  lithium.draggable();
-
-  //Sodium
-  let sodium = createDiv('Na');
-  sodium.position(100,windowHeight-200);
-  sodium.size(50,50);
-  sodium.style('font-size', '50px');
-  sodium.draggable();
-
-  //Potassium
-  let potassium = createDiv('K');
-  potassium.position(100,windowHeight-150);
-  potassium.size(50,50);
-  potassium.style('font-size', '50px');
-  potassium.draggable();
-  
-  //Berryllium
-  let berryllium = createDiv('Be');
-  berryllium.position(170,windowHeight-250);
-  berryllium.size(50,50);
-  berryllium.style('font-size', '50px');
-  berryllium.draggable();
-
-  //Magnesium
-  let magnesium = createDiv('Mg');
-  magnesium.position(170,windowHeight-200);
-  magnesium.size(50,50);
-  magnesium.style('font-size', '50px');
-  magnesium.draggable();
-
-  //Calcium
-  let calcium = createDiv('Ca');
-  calcium.position(170,windowHeight-150);
-  calcium.size(50,50);
-  calcium.style('font-size', '50px');
-  calcium.draggable();
-
-  //Boron
-  let boron = createDiv('B');
-  boron.position(250,windowHeight-250);
-  boron.size(50,50);
-  boron.style('font-size', '50px');
-  boron.draggable();
-
-  //Aluminium
-  let aluminium = createDiv('Al');
-  aluminium.position(250,windowHeight-200);
-  aluminium.size(50,50);
-  aluminium.style('font-size', '50px');
-  aluminium.draggable();
-
-  //Carbon
-  let carbon = createDiv('C');
-  carbon.position(310,windowHeight-250);
-  carbon.size(50,50);
-  carbon.style('font-size', '50px');
-  carbon.draggable();
-
-  //Silicon
-  let silicon = createDiv('Si');
-  silicon.position(310,windowHeight-200);
-  silicon.size(50,50);
-  silicon.style('font-size', '50px');
-  silicon.draggable();
-
-  //Nitrogen
-  let nitrogen = createDiv('N');
-  nitrogen.position(370,windowHeight-250);
-  nitrogen.size(50,50);
-  nitrogen.style('font-size', '50px');
-  nitrogen.draggable();
-
-  //Phosphorus
-  let phosphorus = createDiv('P');
-  phosphorus.position(370,windowHeight-200);
-  phosphorus.size(50,50);
-  phosphorus.style('font-size', '50px');
-  phosphorus.draggable(); 
-
-  //Oxygen
-  let oxygen = createDiv('O');
-  oxygen.position(420,windowHeight-250);
-  oxygen.size(50,50);
-  oxygen.style('font-size', '50px');
-  oxygen.draggable();
-
-  //Sulfur
-  let sulfur = createDiv('S');
-  sulfur.position(420,windowHeight-200);
-  sulfur.size(50,50);
-  sulfur.style('font-size', '50px');
-  sulfur.draggable(); 
-
-  //Fluorine
-  let fluorine = createDiv('F');
-  fluorine.position(470,windowHeight-250);
-  fluorine.size(50,50);
-  fluorine.style('font-size', '50px');
-  fluorine.draggable();
-
-  //Chlorine
-  let chlorine = createDiv('Cl');
-  chlorine.position(470,windowHeight-200);
-  chlorine.size(50,50);
-  chlorine.style('font-size', '50px');
-  chlorine.draggable(); 
-
-  //Helium
-  let helium = createDiv('He');
-  helium.position(520,windowHeight-300);
-  helium.size(50,50);
-  helium.style('font-size', '50px');
-  helium.draggable();
-
-  //Neon
-  let neon = createDiv('Ne');
-  neon.position(520,windowHeight-250);
-  neon.size(50,50);
-  neon.style('font-size', '50px');
-  neon.draggable();
-
-  //Argon
-  let argon = createDiv('Ar');
-  argon.position(520,windowHeight-200);
-  argon.size(50,50);
-  argon.style('font-size', '50px');
-  argon.draggable();
+  startButton.position(windowWidth/2-75, windowHeight/2-20);
 }
 
+function GameStart() {
+  background(237, 39, 36);
+  startButton.remove();
 
-function draw() {
-  stroke(255);
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
+  textSize(100);
+  fill(220);
+  text('Chose your level', (windowWidth/2)-300, 300);
+
+ //Mode selection scene
+  easyMode = createButton("EASY");
+  easyMode.mousePressed(playEasy);
+  easyMode.style("font-size", "30px");
+  easyMode.position(windowWidth/2, windowHeight/2);
+}
+
+function draw(){
+  noFill();
+  background(255);
+  translate(width/2, height/2);
+  circle(0, 0, 10, 10);
+
+  for(i = 0; i < shells.length; i++){
+    circle(0,0,shells[i],shells[i]);
   }
 }
 
-function submitAtoms(){
-
+function mousePressed(){
+  let radius = 30 + 50 * shells.length;
+  shells.push(radius);
 }
+
+// function setup(){
+//   // easyMode.remove();
+
+//   //chemical formulas
+//   // let carbonDioxide = "";
+
+//   createCanvas(windowWidth, windowHeight);
+//   background(102);
+
+//   startButton = createButton("Submit Atoms");
+//   startButton.mousePressed(submitAtoms);
+//   startButton.style("font-family", "Optima bold");
+//   startButton.style("font-size", "30px");
+//   startButton.position(windowWidth/2-75, windowHeight/2);
+//   //Hydrogen
+//   let hydrogen = createDiv('H');
+//   hydrogen.position(100,windowHeight-300);
+//   hydrogen.size(50,50);
+//   hydrogen.style('font-size', '50px');
+//   hydrogen.draggable();
+
+//   //Lithium
+//   let lithium = createDiv('Li');
+//   lithium.position(100,windowHeight-250);
+//   lithium.size(50,50);
+//   lithium.style('font-size', '50px');
+//   lithium.draggable();
+
+//   //Sodium
+//   let sodium = createDiv('Na');
+//   sodium.position(100,windowHeight-200);
+//   sodium.size(50,50);
+//   sodium.style('font-size', '50px');
+//   sodium.draggable();
+
+//   //Potassium
+//   let potassium = createDiv('K');
+//   potassium.position(100,windowHeight-150);
+//   potassium.size(50,50);
+//   potassium.style('font-size', '50px');
+//   potassium.draggable();
+  
+//   //Berryllium
+//   let berryllium = createDiv('Be');
+//   berryllium.position(170,windowHeight-250);
+//   berryllium.size(50,50);
+//   berryllium.style('font-size', '50px');
+//   berryllium.draggable();
+
+//   //Magnesium
+//   let magnesium = createDiv('Mg');
+//   magnesium.position(170,windowHeight-200);
+//   magnesium.size(50,50);
+//   magnesium.style('font-size', '50px');
+//   magnesium.draggable();
+
+//   //Calcium
+//   let calcium = createDiv('Ca');
+//   calcium.position(170,windowHeight-150);
+//   calcium.size(50,50);
+//   calcium.style('font-size', '50px');
+//   calcium.draggable();
+
+//   //Boron
+//   let boron = createDiv('B');
+//   boron.position(250,windowHeight-250);
+//   boron.size(50,50);
+//   boron.style('font-size', '50px');
+//   boron.draggable();
+
+//   //Aluminium
+//   let aluminium = createDiv('Al');
+//   aluminium.position(250,windowHeight-200);
+//   aluminium.size(50,50);
+//   aluminium.style('font-size', '50px');
+//   aluminium.draggable();
+
+//   //Carbon
+//   let carbon = createDiv('C');
+//   carbon.position(310,windowHeight-250);
+//   carbon.size(50,50);
+//   carbon.style('font-size', '50px');
+//   carbon.draggable();
+
+//   //Silicon
+//   let silicon = createDiv('Si');
+//   silicon.position(310,windowHeight-200);
+//   silicon.size(50,50);
+//   silicon.style('font-size', '50px');
+//   silicon.draggable();
+
+//   //Nitrogen
+//   let nitrogen = createDiv('N');
+//   nitrogen.position(370,windowHeight-250);
+//   nitrogen.size(50,50);
+//   nitrogen.style('font-size', '50px');
+//   nitrogen.draggable();
+
+//   //Phosphorus
+//   let phosphorus = createDiv('P');
+//   phosphorus.position(370,windowHeight-200);
+//   phosphorus.size(50,50);
+//   phosphorus.style('font-size', '50px');
+//   phosphorus.draggable(); 
+
+//   //Oxygen
+//   let oxygen = createDiv('O');
+//   oxygen.position(420,windowHeight-250);
+//   oxygen.size(50,50);
+//   oxygen.style('font-size', '50px');
+//   oxygen.draggable();
+
+//   //Sulfur
+//   let sulfur = createDiv('S');
+//   sulfur.position(420,windowHeight-200);
+//   sulfur.size(50,50);
+//   sulfur.style('font-size', '50px');
+//   sulfur.draggable(); 
+
+//   //Fluorine
+//   let fluorine = createDiv('F');
+//   fluorine.position(470,windowHeight-250);
+//   fluorine.size(50,50);
+//   fluorine.style('font-size', '50px');
+//   fluorine.draggable();
+
+//   //Chlorine
+//   let chlorine = createDiv('Cl');
+//   chlorine.position(470,windowHeight-200);
+//   chlorine.size(50,50);
+//   chlorine.style('font-size', '50px');
+//   chlorine.draggable(); 
+
+//   //Helium
+//   let helium = createDiv('He');
+//   helium.position(520,windowHeight-300);
+//   helium.size(50,50);
+//   helium.style('font-size', '50px');
+//   helium.draggable();
+
+//   //Neon
+//   let neon = createDiv('Ne');
+//   neon.position(520,windowHeight-250);
+//   neon.size(50,50);
+//   neon.style('font-size', '50px');
+//   neon.draggable();
+
+//   //Argon
+//   let argon = createDiv('Ar');
+//   argon.position(520,windowHeight-200);
+//   argon.size(50,50);
+//   argon.style('font-size', '50px');
+//   argon.draggable();
+// }
+
+
+// function draw() {
+//   stroke(255);
+//   if (mouseIsPressed === true) {
+//     line(mouseX, mouseY, pmouseX, pmouseY);
+//   }
+// }
+
+// function submitAtoms(){
+
+// }
 // function preload(){
 //   title = loadImage("images/title.jpg");
 // }
